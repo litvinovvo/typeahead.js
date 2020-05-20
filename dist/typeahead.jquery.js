d03a9266b42e192d8c947e6ba4148a2f54cef2ea
Vladimir Litvinov <vlitvinov@dealersocket.com>
2020-05-20 12:00:44 +0200

    update grunt-umd dependency and rebuild

--- dist/typeahead.jquery.js
+++ dist/typeahead.jquery.js
@@ -1,18 +1,19 @@
 /*!
  * typeahead.js 0.11.1
  * https://github.com/twitter/typeahead.js
- * Copyright 2013-2015 Twitter, Inc. and other contributors; Licensed MIT
+ * Copyright 2013-2020 Twitter, Inc. and other contributors; Licensed MIT
  */
 
 (function(root, factory) {
+    if (root === undefined && window !== undefined) root = window;
     if (typeof define === "function" && define.amd) {
         define("typeahead.js", [ "jquery" ], function(a0) {
             return factory(a0);
         });
-    } else if (typeof exports === "object") {
+    } else if (typeof module === "object" && module.exports) {
         module.exports = factory(require("jquery"));
     } else {
-        factory(jQuery);
+        factory(root["jQuery"]);
     }
 })(this, function($) {
     var _ = function() {
